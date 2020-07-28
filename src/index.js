16fc34b027f324f2f773074e3590ccc9fc952fed
Erick Moraes <erickd.moraes@gmail.com>
2020-07-28 16:54:37 -0300

    Sequelize configurated

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const db = require("./models");
+
 const authController = require("./controllers/auth");
 
 const app = express();
@@ -9,6 +11,8 @@ app.get("/", (req, res) => {
     return res.json("API running...");
 });
 
-app.listen(3001, () => {
-    console.log("Listening on port 3001");
+db.sequelize.sync().then(() => {
+    app.listen(3001, () => {
+        console.log("Listening on port 3001");
+    });
 });
